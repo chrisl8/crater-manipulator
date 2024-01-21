dc1bd0ba3a8409409e227fa9a97d9fd5970a9373
LiquidMintLeaf <111805837+MinntLeaf@users.noreply.github.com>
2024-01-21 14:38:03 -0600

    Commented out unfinished particle trigger

--- Items/Map/MapController.gd
+++ Items/Map/MapController.gd
@@ -581,9 +581,10 @@ func MineCellAtPosition(Position: Vector2) -> void:
 		(CompensatedPosition in CurrentData.keys())
 		and (CurrentData[CompensatedPosition] != Vector2i(-1, -1))
 	):
-		TileModificationParticlesController.DestroyCell(
-			CompensatedPosition, CurrentData[CompensatedPosition]
-		)
+		#TileModificationParticlesController.DestroyCell(
+		#	CompensatedPosition, CurrentData[CompensatedPosition]
+		#)
+		pass
 	ModifyCell(CompensatedPosition, Vector2i(-1, -1))
 
 
