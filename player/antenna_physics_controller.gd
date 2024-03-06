591323dc0782849d2b6c43e4942ad446e9c54e67
Christen Lofland <christen@lofland.net>
2024-03-05 19:49:29 -0600

    Formatting.

--- player/antenna_physics_controller.gd
+++ player/antenna_physics_controller.gd
@@ -10,6 +10,7 @@ var MaxVelocity = 40
 
 @export var flipped: bool = false
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for Point in points:
@@ -20,17 +21,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	var Velocity = clampf((global_position.x - LastPosition.x) / delta,-MaxVelocity,MaxVelocity)
+	var Velocity = clampf((global_position.x - LastPosition.x) / delta, -MaxVelocity, MaxVelocity)
 
 	#print(Velocity * DisplacementIntensity)
 
 	var NewPointData = []
 	var Index = 1
-	while  Index < PointsCount:
-		var Offset = -Velocity*DisplacementIntensity * float(Index)*float(Index) * (-1.0 if flipped else 1.0)
-		points[Index].x+=Offset
-		points[Index].x = lerpf(points[Index].x , StartingX[Index], DampingIntensity * delta)
-		Index+=1
+	while Index < PointsCount:
+		var Offset = (
+			-Velocity
+			* DisplacementIntensity
+			* float(Index)
+			* float(Index)
+			* (-1.0 if flipped else 1.0)
+		)
+		points[Index].x += Offset
+		points[Index].x = lerpf(points[Index].x, StartingX[Index], DampingIntensity * delta)
+		Index += 1
 
 	LastPosition = global_position
