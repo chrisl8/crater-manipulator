5549be92e9e15c6ccdf724fe04709946e3cb4442
Christen Lofland <christen@lofland.net>
2024-03-09 15:23:10 -0600

    Clean up code a bit.

--- player/antenna_physics_controller.gd
+++ player/antenna_physics_controller.gd
@@ -1,42 +1,45 @@
 extends Line2D
 
-#Shoudl be exposed in future for general purpose use
-var PointsCount = -1
-var StartingX = []
-var LastPosition
-var DisplacementIntensity = 0.003
-var DampingIntensity = 10
-var MaxVelocity = 40
-
 @export var flipped: bool = false
+
+#Should be exposed in future for general purpose use
+var points_count: int = -1
+var starting_x: Array = []
+var last_position: Vector2
+var displacement_intensity: float = 0.003
+var damping_intensity: int = 10
+var max_velocity: int = 40
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	for Point in points:
-		StartingX.append(Point.x)
-	PointsCount = points.size()
-	LastPosition = global_position
+func _ready() -> void:
+	for point: Vector2 in points:
+		starting_x.append(point.x)
+	points_count = points.size()
+	last_position = global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var Velocity = clampf((global_position.x - LastPosition.x) / delta, -MaxVelocity, MaxVelocity)
+func _process(delta: float) -> void:
+	var new_velocity: float = clampf(
+		(global_position.x - last_position.x) / delta, -max_velocity, max_velocity
+	)
 
-	#print(Velocity * DisplacementIntensity)
+	#print(Velocity * displacement_intensity)
 
-	var NewPointData = []
-	var Index = 1
-	while Index < PointsCount:
-		var Offset = (
-			-Velocity
-			* DisplacementIntensity
-			* float(Index)
-			* float(Index)
+	var point_index: int = 1
+	while point_index < points_count:
+		var offset: float = (
+			-new_velocity
+			* displacement_intensity
+			* float(point_index)
+			* float(point_index)
 			* (-1.0 if flipped else 1.0)
 		)
-		points[Index].x += Offset
-		points[Index].x = lerpf(points[Index].x, StartingX[Index], DampingIntensity * delta)
-		Index += 1
+		points[point_index].x += offset
+		points[point_index].x = lerpf(
+			points[point_index].x, starting_x[point_index], damping_intensity * delta
+		)
+		point_index += 1
 
-	LastPosition = global_position
+	last_position = global_position
